7d50f940d01199711d1b74c7c198fbe18cbff002
Kamsi Nwabueze <knwabuez@usc.edu>
2025-11-29 22:17:24 -0800

    fixed logic

--- rtl/vector_scale.v
+++ rtl/vector_scale.v
@@ -21,6 +21,14 @@ module vector_scale #(parameter WIDTH = 16) (
     wire signed [WIDTH-1:0] V_in2 = V_in[3*WIDTH-1:2*WIDTH];
     wire signed [WIDTH-1:0] V_in3 = V_in[4*WIDTH-1:3*WIDTH];
 
+    wire signed [31:0] scale_num = 32'sd32767;
+    wire signed [31:0] scale_den = {16'b0, max_val};
+    
+    wire signed [31:0] V_out0_scaled = (max_val > 0) ? ((V_in0 * scale_num) / scale_den) : V_in0;
+    wire signed [31:0] V_out1_scaled = (max_val > 0) ? ((V_in1 * scale_num) / scale_den) : V_in1;
+    wire signed [31:0] V_out2_scaled = (max_val > 0) ? ((V_in2 * scale_num) / scale_den) : V_in2;
+    wire signed [31:0] V_out3_scaled = (max_val > 0) ? ((V_in3 * scale_num) / scale_den) : V_in3;
+
     reg done_reg;
     
     always @(posedge clk) begin
@@ -29,11 +37,7 @@ module vector_scale #(parameter WIDTH = 16) (
             done <= 1'b0;
             done_reg <= 1'b0;
         end else if (start) begin
-            if (max_val > 0) begin
-                V_out <= {(V_in3 / max_val), (V_in2 / max_val), (V_in1 / max_val), (V_in0 / max_val)};
-            end else begin
-                V_out <= V_in;
-            end
+            V_out <= {V_out3_scaled[15:0], V_out2_scaled[15:0], V_out1_scaled[15:0], V_out0_scaled[15:0]};
             done_reg <= 1'b1;
             done <= 1'b1;
         end else begin
